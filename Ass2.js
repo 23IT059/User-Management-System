e688c180cef5e2c47d2e691bd232973cd3056835
23IT059 MANGUKIYA YASH MAHESHBHAI <152584683+23IT059@users.noreply.github.com>
2025-02-18 15:23:06 +0530

    Update Ass2.js

--- Ass2.js
+++ Ass2.js
@@ -44,9 +44,7 @@ async function parseBody(req) {
 
 
 const server = http.createServer(async (req, res) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    
 
 
     if (req.method === 'OPTIONS') {
